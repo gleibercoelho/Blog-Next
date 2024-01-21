99c7c98a922bc62d5e4331d37eb64b7e51ff8f15
GFC <gleiberfelipe@hotmail.com>
2024-01-21 07:52:25 -0300

    4th commit

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,27 +1,39 @@
+import PostUser from "@/components/postUser.jsx/postUser";
 import styles from "./singlePost.module.css"
 import Image from "next/image";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+
+  if (!res.ok) {
+    throw new Error("Something went wrong")
+  }
+
+  return res.json()
+}
+
+const SinglePostPage = async ({params}) => {
+
+  const {slug} = params;
+
+  const post = await getData(slug)
   return (
     <div className={styles.container} >
       <div className={styles.imgContainer} >
         <Image src="https://images.pexels.com/photos/8820325/pexels-photo-8820325.jpeg" fill className={styles.img} />
       </div>
       <div className={styles.textContainer} >
-        <h1 className={styles.title} ></h1>
+        <h1 className={styles.title} >{post.title}</h1>
         <div className={styles.detail} >
           <Image src="https://images.pexels.com/photos/8820325/pexels-photo-8820325.jpeg" width={50} height={50} className={styles.avatar} />
-          <div className={styles.detailText} >
-            <span className={styles.detailTitle} >Author</span>
-            <span className={styles.detailValue} >Gleiber Coelho</span>
-          </div>
+         <PostUser userId = {post.userId} />
           <div className={styles.detailText} >
             <span className={styles.detailTitle} >Published</span>
             <span className={styles.detailValue} >16.01.2024</span>
           </div>          
         </div>
         <div className={styles.content}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum fugit sit vero quos culpa modi neque incidunt quae amet sequi ullam, atque aut, corporis ex obcaecati, deleniti soluta recusandae!</p>
+            <p>{post.body}</p>
 
           </div>
       </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,23 +1,35 @@
 import PostCard from '@/components/postCard/postCard';
 import styles from "./blog.module.css"
 
-const  BlogPage = () => {
-    return (
-      <div className={styles.container}>
-        <div className={styles.post}>
-        <PostCard/>
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+
+  if (!res.ok) {
+    throw new Error("Something went wrong")
+  }
+
+  return res.json()
+}
+
+
+
+const BlogPage = async () => {
+
+  const posts = await getData()
+
+  return (
+    <div className={styles.container}>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+
         </div>
-        <div className={styles.post}>
-        <PostCard/>
-        </div>
-        <div className={styles.post}>
-        <PostCard/>
-        </div>
-        <div className={styles.post}>
-        <PostCard/>
-        </div>
-  
-      </div>
-    )
-  };
-  export default BlogPage;
+      ))}
+
+      
+    </div>
+
+
+  )
+};
+export default BlogPage;
